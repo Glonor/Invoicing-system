7c26f1c8848690420647ccf400cdfcdb261bf7b3
Glonor <mouezkhelifi96@gmail.com>
2017-11-14 12:35:02 +0100

    Skeleton app

---
+++ db/migrate/20171114113254_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.text :description
+      t.boolean :billed
+      t.datetime :start
+      t.datetime :end
+      t.references :client, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
