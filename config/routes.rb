010b7b55565df38e84a5586cd32531a7606ff6f3
Glonor <mouezkhelifi96@gmail.com>
2017-11-14 14:56:17 +0100

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
   resources :events
   resources :clients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
